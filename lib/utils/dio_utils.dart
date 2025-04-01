624d3a4ff6b62614bd26f04586762901e7d8c1f9
Gaurav Kumar <gauravkumar15021999@gmail.com>
2025-04-01 17:26:17 +0530

    add methods to save files to gallery and get pictures directory

--- lib/utils/dio_utils.dart
+++ lib/utils/dio_utils.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'dart:typed_data';
 import 'package:dio/dio.dart';
+import 'package:image_gallery_saver_plus/image_gallery_saver_plus.dart';
 import 'package:path_provider/path_provider.dart';
 
 class FileUtils {
@@ -39,5 +40,52 @@ class FileUtils {
   /// Extract file name from URL
   static String _getFileName(String url) {
     return url.split('/').last;
+  }
+
+  static Future<String> getPicturesDirectory() async {
+    if (Platform.isAndroid) {
+      final directory = await getExternalStorageDirectory();
+      if (directory != null) {
+        final picturesDir = Directory('${directory.path}/Pictures');
+        if (!await picturesDir.exists()) {
+          await picturesDir.create(recursive: true);
+        }
+        return picturesDir.path;
+      } else {
+        throw Exception("No external storage directory found");
+      }
+    } else if (Platform.isIOS) {
+      final directory = await getApplicationDocumentsDirectory();
+      return '${directory.path}/Pictures';
+    } else {
+      throw Exception("Unsupported platform");
+    }
+  }
+
+  static Future<bool> saveFileToGallery(
+    String url,
+    String fileName,
+  ) async {
+    var appDocDir = await getExternalStorageDirectory();
+    if (appDocDir == null) {
+      throw Exception("No external storage directory found");
+    }
+    String savePath = "${appDocDir.path}/$fileName";
+    var downloadFile = await Dio().download(url, savePath);
+    if (downloadFile.statusCode != 200) {
+      throw Exception(
+        "Error downloading file: ${downloadFile.statusCode}",
+      );
+    }
+    // Save to gallery
+    final result = await ImageGallerySaverPlus.saveFile(
+      savePath,
+      isReturnPathOfIOS: true,
+    );
+    return result['isSuccess'] == true
+        ? true
+        : throw Exception(
+          "Error saving file to gallery: ${result['error']}",
+        );
   }
 }
